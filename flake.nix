29057bd981f57404437c5c67a6ee43a07bf5447f
Antoine Cotten <hello@acotten.com>
2024-09-06 15:18:46 +0200

    nix: Switch to ruby-nix dev shell
    Creates a fully reproducible development shell with Gem versions derived
    from Gemfile.lock.

--- flake.nix
+++ flake.nix
@@ -1,31 +1,73 @@
 {
   description = "GitHub Pages";
 
-  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz"; # nixos-unstable
-  inputs.flake-schemas.url = "https://flakehub.com/f/DeterminateSystems/flake-schemas/0.1.*.tar.gz";
+  inputs = {
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz"; # nixos-unstable
 
-  outputs = { self, nixpkgs, flake-schemas }:
-    let
-      allSystems = [
-        "x86_64-linux"
-        "aarch64-darwin"
-      ];
+    ruby-nix = {
+      url = "github:inscapist/ruby-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
-        pkgs = import nixpkgs { inherit system; };
-      });
-    in
+    bundix = {
+      url = "github:inscapist/bundix/main";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    flake-utils.url = "https://flakehub.com/f/numtide/flake-utils/0.1.*.tar.gz";
+
+    flake-schemas.url = "https://flakehub.com/f/DeterminateSystems/flake-schemas/0.1.*.tar.gz";
+  };
+
+  outputs =
     {
-      inherit (flake-schemas) schemas;
+      self,
+      nixpkgs,
+      ruby-nix,
+      bundix,
+      flake-utils,
+      flake-schemas,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        # Current 'ruby' package version: 3.3.4
+        # Should match https://github.com/actions/jekyll-build-pages/blob/v1.0.13/Dockerfile
+        pkgs = nixpkgs.legacyPackages.${system};
 
-      devShells = forAllSystems ({ pkgs }: {
-        default = with pkgs; mkShell {
-          packages = [
-            ruby
-            rubyPackages.github-pages
-            rubyPackages.webrick # https://github.com/jekyll/jekyll/pull/8524
-          ];
+        ruby-env =
+          (ruby-nix.lib pkgs {
+            name = "github-pages";
+            gemset = ./gemset.nix;
+          }).env;
+
+        bundix-cli = bundix.packages.${system}.default;
+
+        bundleLock = pkgs.writeShellScriptBin "bundle-lock" ''
+          export BUNDLE_PATH=vendor/bundle
+          bundle lock
+        '';
+        bundleUpdate = pkgs.writeShellScriptBin "bundle-update" ''
+          export BUNDLE_PATH=vendor/bundle
+          bundle lock --update
+        '';
+      in
+      {
+        formatter = pkgs.nixfmt-rfc-style;
+
+        devShells = {
+          default = pkgs.mkShell {
+            buildInputs = [
+              ruby-env
+              bundix-cli
+              bundleLock
+              bundleUpdate
+            ];
+          };
         };
-      });
+      }
+    )
+    // {
+      inherit (flake-schemas) schemas;
     };
 }
